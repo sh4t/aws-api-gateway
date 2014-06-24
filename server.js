ec8eba8ae69b4960ab51ae593868c6c2def70da0
Justin Shattuck <justin@edmodo.com>
2014-06-24 15:27:07 -0700

    adding express framework

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+var express = require('express');
+var app = express();
+
 var AWS = require('aws-sdk');
 var uuid = require('node-uuid');
 
@@ -10,4 +13,12 @@ s3.listBuckets(function(err, data) {
         console.log(err, err.stack); // experienced error
     else
         console.log(data); // success
+});
+
+app.get('/', function(req, res) {
+    res.send('Amazon AWS API Gateway Endpoint');
+});
+
+var server = app.listen(5000, function() {
+    console.log('Listening on port %d', server.address().port);
 });
