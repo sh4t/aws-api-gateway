dbeee0dc39056b042fb87b83e2fe74a944acdfdb
Justin Shattuck <justin@edmodo.com>
2014-06-25 00:40:40 -0700

    added handlebars, added a few prelimnary REST routes

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ var express = require('express');
 var redis = require('redis');
 var db = redis.createClient();
 var app = express();
+var expressHbs = require('express3-handlebars');
+
+app.engine('hbs', expressHbs({extname:'hbs', defaultLayout:'main.hbs'}));
+app.set('view engine', 'hbs');
 
 var AWS = require('aws-sdk');
 var uuid = require('node-uuid');
@@ -9,6 +13,7 @@ var uuid = require('node-uuid');
 AWS.config.loadFromPath('./config.json');
 
 var s3 = new AWS.S3();
+var elb = new AWS.ELB();
 
 s3.listBuckets(function(err, data) {
     if (err)
@@ -32,15 +37,105 @@ app.use(function(req, res, next){
   });
 });
 
-app.get('/', function(req, res) {
-    res.send('Amazon AWS API Gateway Endpoint\n' + '(' + req.online.length + ' users online' + ')');
+app.get('/', function(req, res){
+  res.render('index');
 });
 
-app.get('/listbuckets', function(req, res, next){
+app.get('/buckets', function(req, res, next){
 	var ua = req.headers['user-agent'];
 	db.zadd('online_list_buckets', Date.now(), ua, next);
-	res.send(bucket_list);
+	res.json(bucket_list);
 });
+
+app.get('/bucket/:name/location', function(req, res) {
+	s3.getBucketLocation(params={Bucket: req.params.name}, function(err, data) {
+  	  if (err) console.log(err, err.stack);
+  	  else {
+  		  res.json(data);
+  	  }
+	});
+});
+
+app.get('/bucket/:name/objects', function(req, res) { 
+	var params = {
+	  Bucket: req.params.name, // required
+	  // Delimiter: 'STRING_VALUE',
+	  // EncodingType: 'url',
+	  // Marker: 'STRING_VALUE',
+	  // MaxKeys: 0,
+	  // Prefix: 'STRING_VALUE'
+	};
+	s3.listObjects(params, function(err, data) {
+  	  if (err) console.log(err, err.stack);
+  	  else {
+  		  res.json(data);
+  	  }
+	});
+});
+
+app.get('/bucket/:name/policy', function(req, res) {
+	var params = {
+	  Bucket: req.params.name // required
+	};
+	s3.getBucketPolicy(params, function(err, data) {
+  	  if (err) console.log(err, err.stack);
+  	  else {
+  		  res.json(data);
+  	  }
+	});
+	
+});
+
+app.get('/elbs', function(req, res) {
+	elb.describeLoadBalancers(function(err, data) {
+	  if (err) console.log(err, err.stack);
+	  else {
+		  res.json(data);
+	  }
+	});	
+});
+
+app.get('/elb/:name', function(req, res) {
+	var params = {
+		LoadBalancerNames: [ req.params.name ]
+	}
+	elb.describeLoadBalancers(params, function(err, data) {
+		if (err) console.log(err, err.stack); 
+		else {
+			res.json(data);
+		}
+	});
+});
+
+app.get('/elb/:name/health', function(req, res) {
+	var params = {
+	  LoadBalancerName: req.params.name
+	};
+	elb.describeInstanceHealth(params, function(err, data) {
+		if (err) console.log(err, err.stack); 
+		else {
+			res.json(data);
+		}
+	});
+});
+// app.get('/bucket/:name/lifecycle', function(req, res) {
+// 	s3.getBucketLifecycle(params={Bucket: req.params.name}, function(err, data) {
+// 	  if (err) console.log(err, err.stack); // an error occurred
+// 	  else     res.json(data);           // successful response
+// 	});
+// });
+
+// app.get('/test', function(req, res){
+//
+// 	s3.listBuckets(function(err, data) {
+// 	    if (err)
+// 	        console.log(err, err.stack); // experienced error
+// 	    else
+// 			bucket_list = data
+// 	});
+//
+//   res.render('test', bucket_list);
+// });
 
 var server = app.listen(5000, function() {
     console.log('Listening on port %d', server.address().port);
